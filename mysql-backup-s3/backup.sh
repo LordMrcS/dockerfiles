591c9ff92699c543b5b7af97887ab0b339ca4e2c
Marcos Silva <6921937+LordMrcS@users.noreply.github.com>
2022-10-23 19:16:27 +0000

    fixed some more errors

--- mysql-backup-s3/backup.sh
+++ mysql-backup-s3/backup.sh
@@ -4,10 +4,12 @@ set -eo pipefail
 
 if [ "${S3_ACCESS_KEY_ID}" == "**None**" ]; then
   echo "Error: You did not set the S3_ACCESS_KEY_ID environment variable."
+  exit 1
 fi
 
 if [ "${S3_SECRET_ACCESS_KEY}" == "**None**" ]; then
   echo "Error: You did not set the S3_SECRET_ACCESS_KEY environment variable."
+  exit 1
 fi
 
 if [ "${S3_BUCKET}" == "**None**" ]; then
